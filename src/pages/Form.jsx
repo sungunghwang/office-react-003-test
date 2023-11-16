05e4d184c2b92cc7b2f1efe84e952bf07b45740c
Ungs <onlyhsu3@gmail.com>
2023-11-16 18:14:41 +0900

    16일 두번째 커밋

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,135 +1,139 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+  const [text, setText] = useState('')
+  const [num, setNum] = useState(0)
+  const [tarea, setTarea] = useState('')
+
+  const [sec, setSec] = useState('')
+
   return (
     <div className="container cnt">
       <h1>Form</h1>
 
       {/* 폼 단위 */}
-      <div className="container g-0 text-center mt-5">
-        <div className="row g-0 row-cols-auto">
-          <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-        </div>
-
-        <div className="row row-cols-auto mt-2">
-          <strong>동시출력 : </strong> <span>{`어아`}</span>
-        </div>
+      <div className="mb-5 mt-3">
+        <label htmlFor="text1" className="form-label">
+          글입력
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="text1"
+          name="text1"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value)
+          }}
+        />
+        <p className="mt-3">
+          <strong>출력 : </strong>
+          {text}
+        </p>
       </div>
       {/* 폼 단위 */}
 
       {/* 폼 단위 */}
-      <div className="container g-0 text-center mt-5">
-        <div className="row g-0 row-cols-auto">
-          <div className="col">
-            <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-          </div>
-          <div className="col">
-            <button className="btn btn-primary" type="button" id="button-addon2">
-              Button
-            </button>
-          </div>
+      <div className="mb-5 mt-3">
+        <div className="mb-2">증가감소</div>
+        <div className="d-flex">
+          <button
+            className="btn btn-outline-dark"
+            onClick={() => {
+              setNum((prevNum) => prevNum + 1)
+            }}
+          >
+            1증가
+          </button>
+          <input type="text" className="form-control text-center" style={{ width: '120px' }} id="text2" name="text2" value={num} readOnly />
+          <button
+            className="btn btn-outline-dark"
+            onClick={() => {
+              setNum((prevNum) => prevNum - 1)
+            }}
+          >
+            1감소
+          </button>
         </div>
 
-        <div className="row row-cols-auto mt-2">
-          <strong>동시출력 : </strong> <span>{`어아`}</span>
-        </div>
+        <p className="mt-3">
+          <strong>출력 : </strong>
+          {num}
+        </p>
       </div>
       {/* 폼 단위 */}
 
       {/* 폼 단위 */}
-      <div className="container g-0 text-center mt-5">
-        <div className="row g-0 row-cols-auto">
-          <div className="col">
-            <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-          </div>
-          <div className="col">
-            <button className="btn btn-primary" type="button" id="button-addon2">
-              Button
-            </button>
-          </div>
-        </div>
-
-        <div className="row row-cols-auto mt-2">
-          <strong>동시출력 : </strong> <span>{`어아`}</span>
-        </div>
+      <div className="mb-5 mt-3">
+        <label htmlFor="tarea1" className="form-label">
+          글상자
+        </label>
+        <textarea
+          className="form-control"
+          name="tarea1"
+          id="tarea1"
+          rows="5"
+          onChange={(e) => {
+            setTarea(e.target.value)
+          }}
+        />
+        <p className="mt-3">
+          <strong>출력 : </strong>
+          {tarea}
+        </p>
       </div>
       {/* 폼 단위 */}
 
       {/* 폼 단위 */}
-      <div className="container g-0 text-center mt-5">
-        <div className="row g-0 row-cols-auto">
-          <div className="col">
-            <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-          </div>
-          <div className="col">
-            <button className="btn btn-primary" type="button" id="button-addon2">
-              Button
-            </button>
-          </div>
-        </div>
+      <div className="mb-5 mt-3">
+        <label htmlFor="sec1" className="form-label">
+          셀렉트박스
+        </label>
+        <select
+          id="sec1"
+          className="form-select"
+          value={sec || ''}
+          onChange={(e) => {
+            setSec(e.target.value)
+          }}
+        >
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
 
-        <div className="row row-cols-auto mt-2">
-          <strong>동시출력 : </strong> <span>{`어아`}</span>
-        </div>
+        <p className="mt-3">
+          <strong>출력 : </strong>
+          {sec}
+        </p>
       </div>
       {/* 폼 단위 */}
 
       {/* 폼 단위 */}
-      <div className="container g-0 text-center mt-5">
-        <div className="row g-0 row-cols-auto">
-          <div className="col">
-            <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-          </div>
-          <div className="col">
-            <button className="btn btn-primary" type="button" id="button-addon2">
-              Button
-            </button>
-          </div>
+      <div className="mb-5 mt-3">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+          <label className="form-check-label" for="flexCheckDefault">
+            Default checkbox
+          </label>
+        </div>
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
+          <label className="form-check-label" for="flexCheckChecked">
+            Checked checkbox
+          </label>
         </div>
 
-        <div className="row row-cols-auto mt-2">
-          <strong>동시출력 : </strong> <span>{`어아`}</span>
-        </div>
+        <p className="mt-3">
+          <strong>출력 : </strong>
+          {sec}
+        </p>
       </div>
       {/* 폼 단위 */}
-
-      {/* 폼 단위 */}
-      <div className="container g-0 text-center mt-5">
-        <div className="row g-0 row-cols-auto">
-          <div className="col">
-            <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-          </div>
-          <div className="col">
-            <button className="btn btn-primary" type="button" id="button-addon2">
-              Button
-            </button>
-          </div>
-        </div>
-
-        <div className="row row-cols-auto mt-2">
-          <strong>동시출력 : </strong> <span>{`어아`}</span>
-        </div>
-      </div>
-      {/* 폼 단위 */}
-
-      {/* 폼 단위 */}
-      <div className="container g-0 text-center mt-5">
-        <div className="row g-0 row-cols-auto">
-          <div className="col">
-            <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-          </div>
-          <div className="col">
-            <button className="btn btn-primary" type="button" id="button-addon2">
-              Button
-            </button>
-          </div>
-        </div>
-
-        <div className="row row-cols-auto mt-2">
-          <strong>동시출력 : </strong> <span>{`어아`}</span>
-        </div>
-      </div>
-      {/* 폼 단위 */}
+      <button type="button" className="btn btn-primary">
+        ddd
+      </button>
     </div>
   )
 }
